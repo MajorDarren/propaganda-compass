d1973c01aa09b985134853a108c22a0ad14608f6
MajorDarren <150200484+MajorDarren@users.noreply.github.com>
2026-07-24 20:14:22 +0200

    Adjust similarity threshold and max time for articles

--- scraper/run_scraper.py
+++ scraper/run_scraper.py
@@ -141,8 +141,8 @@ def scrape_and_push():
                         east_articles.append(item)
 
     all_articles = match_articles(west_articles, east_articles,
-                                  similarity_threshold=0.45,
-                                  max_time_hours=6)
+                                  similarity_threshold=0.55,
+                                  max_time_hours=10)
 
     # Remove datetime object before sending JSON
     for article in all_articles:
